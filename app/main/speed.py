920306373d7101717661ec959c065df46cdf60b2
ramza007 <ramza007@gmail.com>
2018-04-27 08:29:35 +0300

    added godspeed test

--- app/main/speed.py
+++ app/main/speed.py
@@ -1,9 +1,13 @@
 import pyspeedtest 
+
+# Test speed function 
+
 st = pyspeedtest.SpeedTest()
 ping = st.ping()
 download = st.download()
 upload = st.upload()
 
+# Print function 
 print('ping: ' + str(ping))
 print('download: ' + str(download))
 print('upload: ' + str(upload))
